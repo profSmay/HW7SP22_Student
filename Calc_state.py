d2680318d2175cab5b92c64d4b01055aa3cffd58
jimsmay <jimsmay@gmail.com>
2022-03-29 14:11:21 -0500

    Modified Calc_State.py to have UnitConverter and to be able to output SatPropsIsobar in SI or English units.

--- Calc_state.py
+++ Calc_state.py
@@ -3,6 +3,182 @@ from scipy.interpolate import griddata
 from scipy.optimize import fsolve
 from pyXSteam.XSteam import XSteam
 
+class UnitConverter():
+    def __init__(self):
+        """
+        This unit converter class is useful for the pipe network and perhaps other problems.
+        The strategy is (number in current units)*(conversion factor)=(number desired units), for instance:
+            1(ft)*(self.ft_to_m) = 1/3.28084 (m)
+            1(in^2)*(self.in2_to_m2) = 1*(1/(12*3.28084))**2 (m^2)
+        """
+
+    """
+    These constants can be used directly from the class without instantiating an object
+    """
+    ft_to_m = 1 / 3.28084
+    ft2_to_m2 = ft_to_m ** 2
+    ft3_to_m3 = ft_to_m ** 3
+    m3_to_ft3=1/ft3_to_m3
+    ft3_to_L = ft3_to_m3 * 1000
+    L_to_ft3 = 1 / ft3_to_L
+    in_to_m = ft_to_m / 12
+    m_to_in = 1 / in_to_m
+    in2_to_m2 = in_to_m ** 2
+    m2_to_in2 = 1 / in2_to_m2
+    g_SI = 9.80665  # m/s^2
+    g_EN = 32.174  # 32.174 ft/s^2
+    gc_EN = 32.174  # lbm*ft/lbf*s^2
+    gc_SI = 1.0  # kg*m/N*s^2
+    lbf_to_kg = 1 / 2.20462
+    kg_to_lbf = 1/lbf_to_kg
+    lbf_to_N = lbf_to_kg * g_SI
+    pa_to_psi = (1 / (lbf_to_N)) * in2_to_m2
+    bar_to_psi = pa_to_psi*100000
+
+    # Energy
+    BTU_to_J=1055.06
+    kJ_to_BTU = 1000/BTU_to_J
+    BTU_to_kJ = 1/kJ_to_BTU
+    kJperkg_to_BTUperlb = kJ_to_BTU/kg_to_lbf
+    m3perkg_to_ft3perlb = m3_to_ft3/kg_to_lbf
+
+    @classmethod  # a classmethod can be used directly from a class without needing to instantiate an object
+    def viscosityEnglishToSI(cls, mu, toSI=True):
+        """
+        Converts between lb*s/ft^2 and Pa*s
+        :param mu: the viscosity in english units
+        :param toSI:  True assumes english in, False assumes SI in
+        :return: the viscosity in Pa*s if toSI=True, lb*s/ft^2 if toSI=False
+        """
+        # (lb*s)/ft^2*((3.3 ft/m)^2)*(1kg/2.2lb)*(9.81m/s^2)->(Pa*s)
+        cf = (1 / cls.ft2_to_m2) * (cls.lbf_to_kg) * cls.g_SI
+        return mu * cf if toSI else mu / cf
+
+    @classmethod  # a classmethod can be used directly from a class without needing to instantiate an object
+    def densityEnglishToSI(cls, rho, toSI=True):
+        """
+        Converts between lb/ft^3 and kg/m^3
+        :param rho: specific weight or density
+        :param toSI:  True assumes english in, False assumes SI in
+        :return: density in SI or EN
+        """
+        # (lb/ft^3)*((3.3ft/m)^3)*(1kg/2.2lb) -> kg/m^3
+        cf = cls.lbf_to_kg / cls.ft3_to_m3
+        return rho * cf if toSI else rho / cf
+
+    @classmethod  # a classmethod can be used directly from a class without needing to instantiate an object
+    def head_to_pressure(cls, h, rho, SI=True):
+        """
+        Convert from height of column of fluid to pressure in consistent units
+        :param h: head in height of fluid (in or m)
+        :return: pressure in (psi or Pa)
+        """
+        if SI:  # p = rho*g*h = g*cf
+            cf = rho * cls.g_SI / cls.gc_SI  # kg*m/m^3*s^2
+            return h * cf
+        else:  # p = rho*g*h = g*cf (h in in)
+            cf = rho * cls.g_EN / cls.gc_EN * (1 / 12) ** 2  # (lbm*ft/ft^3*s^2)(lbf*s^2/lbm*ft)(ft^2/in^2)
+            return h * cf
+        # convert m of water to psi
+        # (m)*(3.3*12in/m)*rho(kg/m^3)*(2.2lb/kg)*(1m/(3.3*12in))^3
+        psi = p * cls.rho * 2.2 / ((3.3 * 12) ** 2)
+        return psi
+
+    @classmethod  # a classmethod can be used directly from a class without needing to instantiate an object
+    def m_to_psi(cls, h, rho):
+        """
+        For converting from height of fluid to psi
+        :param h: height of fluid in m
+        :param rho: density of fluid in kg/m^3
+        :return: pressure in psi
+        """
+        return cls.head_to_pressure(h, rho) * cls.pa_to_psi
+
+    @classmethod  # a classmethod can be used directly from a class without needing to instantiate an object
+    def psi_to_m(cls, p, rho):
+        """
+        For converting from psi to height of fluid.
+        first convert psi to pa
+        :param p: pressure in psi
+        :param rho: density of fluid in kg/m^3
+        :return: height of fluid in m
+        """
+        pa = p / cls.pa_to_psi
+        h = pa / (rho * cls.g_SI)
+        return h
+
+    @classmethod # a classmethod can be used directly from a class without needing to instantiate an object
+    def C_to_F(cls, T=0):
+        return 9/5*(T)+32
+
+    @classmethod # a classmethod can be used directly from a class without needing to instantiate an object
+    def F_to_C(cls, T=0):
+        return 5/9*(T-32)
+
+    @classmethod # a classmethod can be used directly from a class without needing to instantiate an object
+    def K_to_R(cls, T=0):
+        return cls.C_to_F(T-273.15)+459.67
+
+class SatPropsIsobar():
+    def __init__(self, P):
+        """
+        Sets saturation properties for a given isobar
+        :param P:  in kPa
+        """
+        tscol, pscol, hfcol, hgcol, sfcol, sgcol, vfcol, vgcol = np.loadtxt('sat_water_table.txt', skiprows=1, unpack=True)
+        self.PSat=P/100
+        self.TSat=float(griddata(pscol,tscol,self.PSat, method='cubic'))
+        self.hf=float(griddata(pscol, hfcol,self.PSat,method = 'cubic'))
+        self.hg=float(griddata(pscol,hgcol,self.PSat,method = 'cubic'))
+        self.sf=float(griddata(pscol, sfcol,self.PSat,method = 'cubic'))
+        self.sg=float(griddata(pscol,sgcol,self.PSat,method = 'cubic'))
+        self.vf=float(griddata(pscol, vfcol,self.PSat,method = 'cubic'))
+        self.vg=float(griddata(pscol,vgcol,self.PSat,method = 'cubic'))
+        # for doing unit conversions
+        self.UC=UnitConverter()
+        self.getTextOutput()
+
+    def getTextOutput(self, SI=True):
+        """
+        Sets the self.txtOut string for display.
+        :param SI:
+        :return: self.txtOut
+        """
+        if SI is False:
+            P = self.PSat*self.UC.bar_to_psi
+            PUnits="psi"
+            T = self.UC.C_to_F(self.TSat)
+            TUnits="F"
+            hf=self.hf*self.UC.kJperkg_to_BTUperlb
+            hg=self.hg*self.UC.kJperkg_to_BTUperlb
+            HUnits="BTU/lb"
+            sf=self.sf*self.UC.kJperkg_to_BTUperlb/self.UC.K_to_R(T=1)
+            sg=self.sg*self.UC.kJperkg_to_BTUperlb/self.UC.K_to_R(T=1)
+            SUnits="BTU/lb*R"
+            vf=self.vf*self.UC.m3perkg_to_ft3perlb
+            vg=self.vg*self.UC.m3perkg_to_ft3perlb
+            VUnits="ft^3/lb"
+        else:
+            P = self.PSat
+            PUnits = "bar"
+            T = self.TSat
+            TUnits = "C"
+            hf = self.hf
+            hg = self.hg
+            HUnits = "kJ/kg"
+            sf = self.sf
+            sg = self.sg
+            SUnits = "kJ/kg*K"
+            vf = self.vf
+            vg = self.vg
+            VUnits = "m^3/kg"
+
+        self.txtOut = "PSat = {:0.2f} {}, TSat = {:0.2f} {}".format(P, PUnits, T, TUnits)
+        self.txtOut += "\nhf = {:0.2f} {}, hg = {:0.2f} {}".format(hf, HUnits,hg,HUnits)
+        self.txtOut += "\nsf = {:0.2f} {}, sg = {:0.2f} {}".format(sf,SUnits,sg,SUnits)
+        self.txtOut += "\nvf = {:0.4f} {}, vg = {:0.2f} {}".format(vf,VUnits,vg,VUnits)
+        return self.txtOut
+
 class Steam_SI:
     def __init__(self, P=None, T=None, x=None, v=None, h=None, s=None, name=None):
         """
@@ -29,6 +205,7 @@ class Steam_SI:
         self.name = name # a useful identifier
         self.region = None # 'superheated' or 'saturated'
         self.RW=XSteam.specificGasConstant(self)
+        self.calc()
         pass
 
     def calc(self):
